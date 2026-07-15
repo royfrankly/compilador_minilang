f415d105f9af29b3db02da4e465db139f02253f7
ryfrnkly <ryfrnkl_nvrr@outlook.es>
2026-07-15 18:11:01 -0500

    mejoras en el compiladorv3

--- src/parser/ast_nodes.py
+++ src/parser/ast_nodes.py
@@ -1,121 +1,50 @@
-import ply.yacc as yacc
-# Importamos la lista de tokens del analizador léxico (requisito de PLY)
-from src.lexer.lexer import tokens
-# Importamos nuestras clases de nodos AST
-from src.parser.ast_nodes import *
 
-# Precedencia de operadores para resolver ambigüedades matemáticas (Criterio 9)
-# Los operadores más abajo tienen mayor precedencia (se evalúan primero)
-precedence = (
-    ('left', 'OP_Y', 'OP_O'),
-    ('left', 'OP_IGUALDAD', 'OP_DIFERENTE', 'OP_MAYOR', 'OP_MENOR'),
-    ('left', 'OP_SUMA', 'OP_RESTA'),
-    ('left', 'OP_MULT', 'OP_DIV'),
-)
+class NodoAST:
+    pass
 
-# Regla principal: Un programa empieza con INICIO, tiene instrucciones y termina con FIN
-def p_programa(p):
-    '''programa : INICIO lista_instrucciones FIN'''
-    p[0] = Programa(p[2])
+class Programa(NodoAST):
+    def __init__(self, instrucciones):
+        self.instrucciones = instrucciones
 
-# Una lista de instrucciones puede ser una o varias instrucciones seguidas
-def p_lista_instrucciones(p):
-    '''lista_instrucciones : lista_instrucciones instruccion
-                           | instruccion'''
-    if len(p) == 3:
-        p[0] = p[1] + [p[2]] # Agrega la nueva instrucción a la lista
-    else:
-        p[0] = [p[1]]        # Inicia una nueva lista
+class Declaracion(NodoAST):
+    def __init__(self, tipo_dato, id_variable):
+        self.tipo_dato = tipo_dato
+        self.id_variable = id_variable
 
-# Una instrucción puede ser una declaración, asignación, ciclo, condicional o E/S
-def p_instruccion(p):
-    '''instruccion : declaracion
-                   | asignacion
-                   | condicional
-                   | ciclo
-                   | lectura
-                   | escritura'''
-    p[0] = p[1]
+class Asignacion(NodoAST):
+    def __init__(self, id_variable, expresion):
+        self.id_variable = id_variable
+        self.expresion = expresion
 
-# Regla para declarar variables: (ej. ENTERO x;)
-def p_declaracion(p):
-    '''declaracion : tipo_dato ID PUNTO_COMA'''
-    p[0] = Declaracion(p[1], p[2])
+class OperacionBinaria(NodoAST):
+    def __init__(self, izquierda, operador, derecha):
+        self.izquierda = izquierda
+        self.operador = operador
+        self.derecha = derecha
 
-def p_tipo_dato(p):
-    '''tipo_dato : ENTERO
-                 | REAL
-                 | CADENA
-                 | BOOLEANO'''
-    p[0] = p[1]
+class Numero(NodoAST):
+    def __init__(self, valor):
+        self.valor = valor
 
-# Regla para asignación: (ej. x = 5 + y;)
-def p_asignacion(p):
-    '''asignacion : ID OP_ASIG expresion PUNTO_COMA'''
-    p[0] = Asignacion(p[1], p[3])
+class Identificador(NodoAST):
+    def __init__(self, nombre):
+        self.nombre = nombre
 
-# Regla para operaciones matemáticas y lógicas
-def p_expresion_operacion(p):
-    '''expresion : expresion OP_SUMA expresion
-                 | expresion OP_RESTA expresion
-                 | expresion OP_MULT expresion
-                 | expresion OP_DIV expresion
-                 | expresion OP_MAYOR expresion
-                 | expresion OP_MENOR expresion
-                 | expresion OP_IGUALDAD expresion
-                 | expresion OP_DIFERENTE expresion
-                 | expresion OP_Y expresion
-                 | expresion OP_O expresion'''
-    p[0] = OperacionBinaria(p[1], p[2], p[3])
+class CondicionalSi(NodoAST):
+    def __init__(self, condicion, bloque_si, bloque_sino=None):
+        self.condicion = condicion
+        self.bloque_si = bloque_si
+        self.bloque_sino = bloque_sino
 
-# Expresiones entre paréntesis para alterar precedencia
-def p_expresion_agrupacion(p):
-    '''expresion : PAR_IZQ expresion PAR_DER'''
-    p[0] = p[2]
+class CicloMientras(NodoAST):
+    def __init__(self, condicion, bloque):
+        self.condicion = condicion
+        self.bloque = bloque
 
-# Valores literales y variables que pueden ser parte de una expresión
-def p_expresion_valores(p):
-    '''expresion : NUM_ENTERO
-                 | NUM_REAL
-                 | CADENA_LITERAL
-                 | VERDADERO
-                 | FALSO'''
-    p[0] = Numero(p[1]) # Agrupamos todos bajo "Numero" o valor crudo
+class Leer(NodoAST):
+    def __init__(self, id_variable):
+        self.id_variable = id_variable
 
-def p_expresion_id(p):
-    '''expresion : ID'''
-    p[0] = Identificador(p[1])
-
-# Regla para la condición SI...SINO
-def p_condicional(p):
-    '''condicional : SI PAR_IZQ expresion PAR_DER lista_instrucciones FIN_SI
-                   | SI PAR_IZQ expresion PAR_DER lista_instrucciones SINO lista_instrucciones FIN_SI'''
-    if len(p) == 7: # Sin bloque SINO
-        p[0] = CondicionalSi(p[3], p[5])
-    else:           # Con bloque SINO
-        p[0] = CondicionalSi(p[3], p[5], p[7])
-
-# Regla para el ciclo MIENTRAS
-def p_ciclo(p):
-    '''ciclo : MIENTRAS PAR_IZQ expresion PAR_DER lista_instrucciones FIN_MIENTRAS'''
-    p[0] = CicloMientras(p[3], p[5])
-
-# Reglas para Entrada y Salida
-def p_lectura(p):
-    '''lectura : LEER ID PUNTO_COMA'''
-    p[0] = Leer(p[2])
-
-def p_escritura(p):
-    '''escritura : ESCRIBIR expresion PUNTO_COMA'''
-    p[0] = Escribir(p[2])
-
-# Función para manejar errores de sintaxis
-def p_error(p):
-    if p:
-        print(f"Error Sintáctico: Token inesperado '{p.value}' cerca de la línea {p.lineno}")
-    else:
-        print("Error Sintáctico: Fin de archivo inesperado")
-
-# Instanciación del parser
-def obtener_parser():
-    return yacc.yacc()
+class Escribir(NodoAST):
+    def __init__(self, expresion):
+        self.expresion = expresion
